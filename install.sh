5f1e704a69ed2b80fcc1d5ab37cd6b17b7f17920
djp <davamundo@gmail.com>
2015-09-24 16:24:27 -0400

    better dialogs

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#
+# install.sh
+# Copyright (C) 2015 djp <djp@transit>
+#
+# Distributed under terms of the MIT license.
+#
+
+# Taskwarrior Time of Day Shell Script -- install script
+
